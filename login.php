0880ed713a52caf8752c0061fc1b894b7ad946a2
jeremy <ayumi2183@gmail.com>
2018-08-02 19:15:55 +0800

    update login.php

--- login.php
+++ login.php
@@ -1,18 +1,10 @@
 <?php
 $memId = $_POST['memId'];
 $memPsw = $_POST['memPsw'];
-
-$ch = curl_init();
-$url = "http://35.197.87.247/jeremy/php_test/testmachine/member.txt";
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true ); 
-$result = curl_exec($ch);
-
+require_once('member.php');   //會員帳密
 $sev = explode(',',$result);
 // print_r($sev);
-
 // var_dump($sev);
-
  $cus = array("id"=>$sev[0],"psw"=>$sev[1],"id2"=>$sev[2],"psw2"=>$sev[3],"id3"=>$sev[4],"psw3"=>$sev[5],"id4"=>$sev[6],"psw4"=>$sev[7]);
  
 
@@ -28,29 +20,21 @@ if(isset($_POST["memId"]) === true){
 		if(isset($_SESSION["UrlRedirect"])){
 			$redir = $_SESSION["UrlRedirect"];
 		}else{
-			
+				$date = date("Y/m/d h:i:sa");
+				$day = date("Ymd");
 			 // 寫入登入者資料到list2.txt
+
 			if ($_POST["memId"] == $cus["id"]){
-				 	$date = date("Y/m/d h:i:sa");
-				 	$day = date("Ymd");
-	      	$new_line = implode(array($cus["id"])); 
-	      	file_put_contents($day.".log", "、 登入者：".$new_line ." ".$date." ", FILE_APPEND);   // file_put_contents(把字串寫入文件中)
+	      	$new_line2 = implode(array($cus["id"])); 
        }else if ($_POST["memId"] == $cus["id2"]){
-	       	$date = date("Y/m/d h:i:sa");
-	       	$day = date("Ymd");
 	      	$new_line2 = implode(array($cus["id2"])); 
-	      	file_put_contents($day.".log", "、 登入者：".$new_line2 ." ".$date." ", FILE_APPEND); 
        }else if ($_POST["memId"] == $cus["id3"]){
-	       	$date = date("Y/m/d h:i:sa");
-	       	$day = date("Ymd");
 	      	$new_line2 = implode(array($cus["id3"])); 
-	      	file_put_contents($day.".log", "、 登入者：".$new_line2 ." ".$date." ", FILE_APPEND); 
+	    
        }else if ($_POST["memId"] == $cus["id4"]){
-	       	$date = date("Y/m/d h:i:sa");
-	       	$day = date("Ymd");
 	      	$new_line2 = implode(array($cus["id4"])); 
-	      	file_put_contents($day.".log", "、 登入者：".$new_line2 ." ".$date." ", FILE_APPEND); 
        }
+       file_put_contents($day.".log", "、 登入者：".$new_line2 ." ".$date." ", FILE_APPEND); // file_put_contents(把字串寫入文件中)
    		
 
 			echo "<script>
